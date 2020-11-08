db4d2f3448e6fed12344931727b1c35d60307bf0
chaitanya poosapathi <chaitanyaraju.1995@gmail.com>
2020-11-08 00:52:15 -0500

    complete working edit

---
+++ file.tf
@@ -0,0 +1,19 @@
+resource "aws_spot_instance_request" "cheap_worker" {
+  ami           = "ami-0dbd6ff6270bd154d"
+  spot_price    = "0.02"
+  instance_type = "t2.medium"
+  root_block_device {
+    volume_size = "25"
+  }
+  key_name = "ec2"
+  user_data = file("script.sh")
+  security_groups = ["launch-wizard-4"]
+
+  tags = {
+    Name = "CheapWorker"
+  }
+}
+
+output "ipadress" {
+  value = aws_spot_instance_request.cheap_worker.public_ip
+}
---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "aws" {  
+  region = "ca-central-1"
+}
+
+# provider "aws" {
+#   region = "ca-central-1"
+#   alias = "ca-central"
+# }
